c98aa7bb564a86db3a1f29fc1b70adbdbc383f48
Jonghakseo <unqocn@gmail.com>
2022-09-11 17:48:48 +0900

    함수 분리

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -3,35 +3,57 @@ import getCommitElements from "@pages/Content/getCommitElements";
 import createCommitsCollapse from "@pages/Content/createCommitsCollapse";
 import removeAlreadyCreatedDetail from "@pages/Content/removeAlreadyCreatedDetail";
 
-const updateCommits = (target: HTMLElement) => {
-  const toolbar = select("markdown-toolbar", target);
-  removeAlreadyCreatedDetail(toolbar);
-  toolbar?.appendChild(createCommitsCollapse(getCommitElements()));
+const findToolbar =
+  (htmlElement: HTMLElement) => (callback: (toolbar: HTMLElement) => void) => {
+    const toolbar = select("markdown-toolbar", htmlElement);
+    if (!toolbar) {
+      return;
+    }
+    callback(toolbar);
+  };
+
+const updateCommits = (htmlElement: HTMLElement) => {
+  removeAlreadyCreatedDetail(htmlElement);
+  htmlElement.appendChild(createCommitsCollapse(getCommitElements()));
 };
 
-const detect = (targetElement: HTMLElement) => {
-  if (!targetElement) {
-    return;
-  }
-  updateCommits(targetElement);
+const findToolbarAndUpdateCommits = (htmlElement: HTMLElement) =>
+  findToolbar(htmlElement)(updateCommits);
 
+const observeElement = (element: HTMLElement, callback: () => void) => {
   const observer = new MutationObserver(function (mutations) {
-    mutations.forEach(() => updateCommits(targetElement));
+    mutations.forEach(callback);
   });
 
-  observer.observe(targetElement, {
+  observer.observe(element, {
     childList: true,
   });
 };
 
-function init() {
-  for (const toolbar of select.all("markdown-toolbar")) {
-    detect(toolbar);
-    toolbar.appendChild(createCommitsCollapse(getCommitElements()));
+const detect = (targetElement: HTMLElement) => {
+  findToolbarAndUpdateCommits(targetElement);
+
+  observeElement(targetElement, () => {
+    findToolbarAndUpdateCommits(targetElement);
+  });
+};
+
+function initBottomCommentBox() {
+  const bottomCommentBox = select("#issue-comment-box");
+  if (!bottomCommentBox) {
+    return;
   }
+  findToolbarAndUpdateCommits(bottomCommentBox);
+}
+
+function init() {
+  initBottomCommentBox();
 
   const detectTargets = select.all("div.js-discussion");
   detectTargets.map((target) => {
+    if (!target) {
+      return;
+    }
     detect(target);
   });
 }
