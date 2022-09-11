7774ce0f30970ef557fe2240ceaeafaea3ea49b4
Jonghakseo <unqocn@gmail.com>
2022-09-11 18:11:07 +0900

    사용하지 않는 코드 정리

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -3,30 +3,14 @@ import getCommitElements from "@pages/Content/getCommitElements";
 import createCommitsCollapse from "@pages/Content/createCommitsCollapse";
 import removeAlreadyCreatedDetail from "@pages/Content/removeAlreadyCreatedDetail";
 
-const findToolbar = (htmlElement: HTMLElement): HTMLElement | null => {
-  const toolbar = select("markdown-toolbar", htmlElement);
-  if (!toolbar) {
-    return null;
-  }
-  return toolbar;
-};
-
 const findAllToolbar = (): HTMLElement[] => {
   const toolbars = select.all("markdown-toolbar");
   return toolbars.filter(Boolean);
 };
 
-const updateCommits = (htmlElement: HTMLElement) => {
+const updateCommits = (htmlElement: HTMLElement): void => {
   removeAlreadyCreatedDetail(htmlElement);
   htmlElement.appendChild(createCommitsCollapse(getCommitElements()));
-};
-
-const findToolbarAndUpdateCommits = (htmlElement: HTMLElement) => {
-  const toolbar = findToolbar(htmlElement);
-  if (!toolbar) {
-    return;
-  }
-  updateCommits(toolbar);
 };
 
 const observeElement = (element: HTMLElement, callback: () => void) => {
@@ -39,66 +23,26 @@ const observeElement = (element: HTMLElement, callback: () => void) => {
   });
 };
 
-const initDiscussion = (htmlElement: HTMLElement) => {
-  findToolbarAndUpdateCommits(htmlElement);
-
-  observeElement(htmlElement, () => {
-    findToolbarAndUpdateCommits(htmlElement);
-  });
-};
-
-const getDiscussions = (): HTMLElement[] => {
+const getCommitUpdateArea = (): HTMLElement[] => {
   const discussions = select.all("div.js-discussion");
   return discussions.filter(Boolean);
 };
 
-function initDiscussions() {
-  const discussions = getDiscussions();
-  discussions.forEach(initDiscussion);
-}
-
-const getBottomCommentBox = (): HTMLElement | null => {
-  const bottomCommentBox = select("#issue-comment-box");
-  if (!bottomCommentBox) {
-    return null;
-  }
-
-  return bottomCommentBox;
-};
-
-function initBottomCommentBox() {
-  const bottomCommentBox = getBottomCommentBox();
-  if (!bottomCommentBox) {
-    return;
-  }
-  findToolbarAndUpdateCommits(bottomCommentBox);
-  observeElement(bottomCommentBox, () => {
-    findToolbarAndUpdateCommits(bottomCommentBox);
-  });
-}
-
-function updateAllToolbar() {
+function updateAllToolbar(): void {
   const allToolbar = findAllToolbar();
   allToolbar.forEach(updateCommits);
 }
 
-function init() {
-  updateAllToolbar();
-  whenCommitUpdated(updateAllToolbar);
-
-  // initBottomCommentBox();
-  // initDiscussions();
-}
-
-function whenCommitUpdated(callback: () => void) {
-  const discussions = getDiscussions();
-  discussions.forEach((discussion) => {
-    observeElement(discussion, callback);
+function whenCommitUpdated(callback: () => void): void {
+  const commitUpdateArea = getCommitUpdateArea();
+  commitUpdateArea.forEach((element) => {
+    observeElement(element, callback);
   });
 }
 
-// observe 하는 부분은 discussion
-
-// 업데이트 해야 하는 부분들은 툴바
+function init(): void {
+  updateAllToolbar();
+  whenCommitUpdated(updateAllToolbar);
+}
 
 init();
